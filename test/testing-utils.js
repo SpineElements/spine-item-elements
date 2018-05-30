981c76437fa00ab379de5b8fb197950684de1f7a
Dmitry Pikhulya <dmitry.pikhulya@teamdev.com>
2018-05-30 18:38:11 +0300

    Review the auto-generated code style in tests

--- test/testing-utils.js
+++ test/testing-utils.js
@@ -1,8 +1,8 @@
 /*
-  ~ Copyright (c) 2000-2018 TeamDev Ltd. All rights reserved.
-  ~ TeamDev PROPRIETARY and CONFIDENTIAL.
-  ~ Use is subject to license terms.
-  */
+ * Copyright (c) 2000-2018 TeamDev. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
 import '@polymer/iron-test-helpers/iron-test-helpers.js';
 
 /*
